4c9d0fe5087dd722ab5f648d87f8b36632154d81
Jana-24 <124114156+Jana-24@users.noreply.github.com>
2024-08-24 08:55:22 +0530

    leap year.py

--- leap.py
+++ leap.py
@@ -6,7 +6,14 @@ if a%4==0:
             print("Given anniversary year: leap year")
         else:
             print("Given anniversary year: non leap year")
+            for i in range(a,a-5,-1):
+                if i%4==0:
+                    print("the anniversary date is ",doa[0:5],i)
+                
     else:
         print("Given anniversary year: leap year")
 else:
     print("Given anniversary year: non leap year")
+    for i in range(a,a-5,-1):
+        if i%4==0:
+            print("the anniversary date is ",doa[0:6],i)
